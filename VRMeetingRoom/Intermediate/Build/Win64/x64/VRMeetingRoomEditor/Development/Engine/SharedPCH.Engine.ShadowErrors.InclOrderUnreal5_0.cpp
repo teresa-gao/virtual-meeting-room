c6c4ba4b71ae576556a244fa333d399fc4c3b850
Anirudh N <naduhrin78@gmail.com>
2023-11-14 23:21:41 +0000

    fix: ignore and stop tracking binaries, intermediate an dcache

--- VRMeetingRoom/Intermediate/Build/Win64/x64/VRMeetingRoomEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.InclOrderUnreal5_0.cpp
+++
@@ -1,2 +0,0 @@
-// Compiler: 14.34.31948
-#include "C:/Users/naduh/Trinity_College_Dublin/virtual-meeting-room/VRMeetingRoom/Intermediate/Build/Win64/x64/VRMeetingRoomEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.InclOrderUnreal5_0.h"
