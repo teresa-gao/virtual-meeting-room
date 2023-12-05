f860fb81e481aa35c62174bf710bce2cdc312e23
Lumi_V <bawagupta69@gmail.com>
2023-12-05 02:20:52 +0000

    Meeting Room and Set shading mode to Lumen

---
+++ VRMeetingRoom/Source/VRMeetingRoom/baseClass.cpp
@@ -0,0 +1,13 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "baseClass.h"
+#include "GameFramework/Actor.h"
+
+baseClass::baseClass()
+{
+}
+
+baseClass::~baseClass()
+{
+}
